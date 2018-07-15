800b01eeeb2294e886b97f2cd3edf5da5230dbf9
niefeng0709 <1041165942@qq.com>
2018-07-15 20:54:12 +0800

    ddddd

---
+++ src/main/java/demo/msa/service/HelloService.java
@@ -0,0 +1,11 @@
+package demo.msa.service;
+
+/**
+ * @author niexy
+ * @version 1.0.0
+ * @ClassName HelloService
+ * @Description TODO
+ * @createTime 2018年07月15日
+ */
+public class HelloService {
+}
